f8fcd08c930d2a34b086c2322214278462210aad
Gergely Tonté <gtonte@gmail.com>
2015-10-08 21:23:14 +0200

    Initial data added.

---
+++ projects/SleepTracker/initial-data.sql
@@ -0,0 +1,8 @@
+INSERT INTO USER(FirstName, LastName, BirthDate, Height, Weight, Sex) VALUES ("Várady", "László", '1993-12-18', 180, 60, true);
+INSERT INTO Sleep(SleepID, StartTime, EndTime, RatingID, MealID, SportID, DateOfDay) VALUES ('11:30', '8:30', 1,1,1,'2015-10-08');
+INSERT INTO Rating(RatingID, Relaxed, Feel) VALUES (8, 1);
+INSERT INTO Meal(MealID, MealTime, MealContentL, Fullness) VALUES ('9:00', "Pacal pürkütt", 10);
+INSERT INTO Sport(SportID, Length, Type, Exhaustion) VALUES (60, 1, 7);
+INSERT INTO Feel(FeelID, FeelValue) VALUES ("Kipihent");
+INSERT INTO DateID Day(Date, Tempreature, FrontImpact, Humidity, Rain) VALUES ('2015-10-08', 15, "Hidegfront, fáj a lábujjam.", 50, true );
+INSERT INTO Type(TypeID, TypeValue) VALUES (1, "Futás");
