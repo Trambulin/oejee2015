9f52a3c53de4dfd84d04ac5a0c7edbf72d805dd7
kicsiZii <zelenkagabor@gmail.com>
2015-10-21 13:39:07 -0700

    SleepQuery final

--- projects/SleepTracker/sas-persistence/src/main/java/hu/sas/persistance/query/SleepQuery.java
+++ projects/SleepTracker/sas-persistence/src/main/java/hu/sas/persistance/query/SleepQuery.java
@@ -1,5 +1,8 @@
 package hu.sas.persistance.query;
 
 public class SleepQuery {
-
+	public static final String GET_BY_SleepID = "Sleep.getBySleepId";
+	public static final String GET_BY_UserID = "Sleep.getByUserId";
+	public static final String GET_BY_Date = "Sleep.getByDate";
+	public static final String GET_ALL = "Book.getAll";	
 }
