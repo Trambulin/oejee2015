bd5263cd7658b393d256cdd57c59592a1c48241e
kicsiZii <zelenkagabor@gmail.com>
2015-10-21 21:30:20 -0700

    test

---
+++ projects/SleepTracker/sas-persistence/model/create-user.sql
@@ -0,0 +1,3 @@
+CREATE USER sas_user;
+ALTER USER sas_user PASSWORD 'makdaralo';
+GRANT sas_role TO sas_user;
---
+++ projects/SleepTracker/sas-persistence/model/grant-access.sql
@@ -0,0 +1,1 @@
+GRANT SELECT, INSERT, UPDATE, DELETE ON Sleep, Day, Rating, Feel, Sport, Type, Meal, User_account TO sas_role;
