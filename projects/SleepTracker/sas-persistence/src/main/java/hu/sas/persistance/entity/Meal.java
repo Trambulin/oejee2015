fcabec6249b0bbaa4f19c8b46d6d9e3b1e9e89a9
Gergely Tonté <gtonte@gmail.com>
2015-10-22 11:54:17 +0200

    asd

--- projects/SleepTracker/sas-persistence/src/main/java/hu/sas/persistance/entity/Meal.java
+++ projects/SleepTracker/sas-persistence/src/main/java/hu/sas/persistance/entity/Meal.java
@@ -1,9 +1,30 @@
 package hu.sas.persistance.entity;
 
 import java.io.Serializable;
+import java.util.Date;
+
 
 public class Meal implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3228698017701007483L;
 	
+	@Id
+	@SequenceGenerator(name = "generatorMeal", sequenceName = "meal_MealID_seq", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "generatorMeal")
+	@Column(name = "MealID", nullable = false, updatable = false, insertable = false)
+	private Long id;
+	
+	@Column(name = "MealTime", nullable = false)
+	private Date mealTime;
+	
+	@Column(name = "MealContent", nullable = false)
+	private String mealContent;
+	
+	@Column(name = "Fullness", nullable = false)
+	private int fullness;
 	
 
 }
