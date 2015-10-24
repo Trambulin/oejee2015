fcd357a7fe559ac057d07f18c3a5fd0dbaa51a66
fviktor93 <f.viktor93@gmail.com>
2015-10-24 22:36:03 +0200

    [ADD] missing grants

--- projects/Ourapplication/condominium/cm-persistence/model/grant-access.sql
+++ projects/Ourapplication/condominium/cm-persistence/model/grant-access.sql
@@ -1,4 +1,4 @@
-GRANT SELECT, INSERT, UPDATE, DELETE ON c_type, condominium, c_owner, repair TO condominium_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON c_type, condominium, c_owner, repair,condominium_owner,c_user,notification,repair_notification TO condominium_role;
     
 GRANT USAGE, SELECT, UPDATE ON c_type_c_type_id_seq TO condominium_role;
 GRANT USAGE, SELECT, UPDATE ON condominium_condominium_id_seq TO condominium_role;
