1a1d95123080338f98d664075ff58091d0157ab9
peterhidvegi <peter.hidvegi@epsglobal.eu>
2015-10-29 01:23:40 +0100

    ADD userStub,Converter

---
+++ projects/Ourapplication/condominium/cm-ejbservice/src/main/java/hu/nik/condominium/ejbservice/converter/UserConverter.java
@@ -0,0 +1,5 @@
+package hu.nik.condominium.ejbservice.converter;
+
+public interface UserConverter {
+
+}
---
+++ projects/Ourapplication/condominium/cm-ejbservice/src/main/java/hu/nik/condominium/ejbservice/domain/UserStub.java
@@ -0,0 +1,39 @@
+package hu.nik.condominium.ejbservice.domain;
+
+public class UserStub {
+
+	private String login;
+	private String pass;
+	private String email;
+	private String fullName;
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	
+	@Override
+	public String toString() {
+		return "Login: "+ this.login+", Password: "+this.pass+", Email: "+this.email+", Name: "+this.fullName;
+	}
+}
