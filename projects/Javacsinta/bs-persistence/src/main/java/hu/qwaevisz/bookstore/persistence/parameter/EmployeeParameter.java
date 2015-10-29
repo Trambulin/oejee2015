c06369bdd5e658b464a167b245f0b4b3be68d3d6
Esse Zsolt <esse.zs10@gmail.com>
2015-10-29 15:09:35 +0100

    persistence layer

---
+++ projects/Javacsinta/bs-persistence/src/main/java/hu/qwaevisz/bookstore/persistence/parameter/EmployeeParameter.java
@@ -0,0 +1,11 @@
+package hu.qwaevisz.bookstore.persistence.parameter;
+
+public class EmployeeParameter {
+
+	public static final String ID = "id";
+	public static final String ADDRESS_ID = "address_id";
+	public static final String NAME = "name";
+	public static final String PHONE = "phone";
+	public static final String OTHER_DETAILS = "other_details";
+
+}
---
+++ projects/Javacsinta/bs-persistence/src/main/java/hu/qwaevisz/bookstore/persistence/parameter/OrderMasterParameter.java
@@ -0,0 +1,10 @@
+package hu.qwaevisz.bookstore.persistence.parameter;
+
+public class OrderMasterParameter {
+
+	public static final String ID = "id";
+	public static final String CUSTOMER_ID = "customer_id";
+	public static final String EMPLOYEE_ID = "employee_id";
+	public static final String DELIVERY_STATUS_ID = "delivery_status_id";
+	public static final String TOTAL_PRICE = "total_price";
+}
