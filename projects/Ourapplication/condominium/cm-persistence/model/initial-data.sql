fdd7d63b913820c8d2235df7bea3c844985c08b4
fviktor93 <f.viktor93@gmail.com>
2015-11-08 19:23:20 +0100

    [add] some test record

--- projects/Ourapplication/condominium/cm-persistence/model/initial-data.sql
+++ projects/Ourapplication/condominium/cm-persistence/model/initial-data.sql
@@ -12,6 +12,10 @@ INSERT INTO c_owner (c_owner_name,c_owner_birt_date,c_owner_gender, c_owner_phon
 INSERT INTO repair (repair_con_id,repair_description,repair_price,repair_date) VALUES (1,'desc1',123000,'2005-07-13');
 INSERT INTO repair (repair_con_id,repair_description,repair_price,repair_date) VALUES (2,'desc2',90000,'2005-07-13');
 INSERT INTO repair (repair_con_id,repair_description,repair_price,repair_date) VALUES (1,'desc3',123700,'2005-07-13');
+insert into repair (repair_con_id, repair_description,repair_price,repair_date) values(2,'paint',300000,'2012-08-01');
+insert into repair (repair_con_id, repair_description,repair_price,repair_date) values(2,'painting',200000,'2015-08-01');
+insert into repair (repair_con_id, repair_description,repair_price,repair_date) values(1,'nothing',80000,'2013-08-01');
+
 
 INSERT INTO c_user (c_user_login,c_user_pass,c_user_e_mail,c_user_full_name) VALUES ('boss','s2e5a1E2asd21','boss@cond.com','B B');
 INSERT INTO c_user (c_user_login,c_user_pass,c_user_e_mail,c_user_full_name) VALUES ('employee1','s2e5dssdE2asd21','e1@cond.com','E 1');
