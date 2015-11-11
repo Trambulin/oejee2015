b7c2c617d551a44f670ea1adff5b789ddb7af34b
fviktor93 <f.viktor93@gmail.com>
2015-11-12 00:05:59 +0100

    [add] missing typeQuery

---
+++ projects/Ourapplication/condominium/cm-persistence/src/main/java/hu/nik/condominium/persistence/query/TypeQUery.java
@@ -0,0 +1,7 @@
+package hu.nik.condominium.persistence.query;
+
+
+public class TypeQuery {
+    public static final String GET_BY_NAME = "CondominiumType.getByName";
+    public static final String GET_BY_ID = "CondominiumType.getById";
+}
