ef1a91368b9ca0457f6b2c16e6cd18f63cae5cfe
peterhidvegi <peter.hidvegi@epsglobal.eu>
2015-11-28 23:44:03 +0100

    [DEL] refactoring...

--- projects/Ourapplication/condominium/cm-persistence/src/main/java/hu/nik/condominium/persistence/query/TypeQUery.java
+++
@@ -1,7 +0,0 @@
-package hu.nik.condominium.persistence.query;
-
-
-public class TypeQuery {
-    public static final String GET_BY_NAME = "CondominiumType.getByName";
-    public static final String GET_BY_ID = "CondominiumType.getById";
-}
