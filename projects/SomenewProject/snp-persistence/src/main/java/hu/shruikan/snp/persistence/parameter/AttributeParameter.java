8c2a0e100ca1eca6f7f9026d914428f1ec50c01e
Takács Dániel <daniel.takacs2569@gmail.com>
2015-12-09 13:45:15 +0100

    [DEL] Useless snp-ejbservice, snp-persistence & snp-weblayer

--- projects/SomenewProject/snp-persistence/src/main/java/hu/shruikan/snp/persistence/parameter/AttributeParameter.java
+++
@@ -1,7 +0,0 @@
-package hu.shruikan.snp.persistence.parameter;
-
-public class AttributeParameter 
-{
-	public static final String ID = "id";
-	public static final String PRODUCT_ID = "productId";
-}
