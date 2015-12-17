b563f6a5408aaebe446dc63f4110cc14cb288dee
Esse Zsolt <esse.zs10@gmail.com>
2015-12-17 11:54:15 +0100

    [ADD][CHG][FIX] Javacsinta: basic web interface

--- projects/Javacsinta/jcs-weblayer/src/main/java/hu/oe/pancakestore/weblayer/servlet/PancakeListView.java
+++ projects/Javacsinta/jcs-weblayer/src/main/java/hu/oe/pancakestore/weblayer/servlet/PancakeListView.java
@@ -14,29 +14,33 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 
 
-import hu.oe.pancakestore.serviceclient.domain.CustomerStub;
+import hu.oe.pancakestore.serviceclient.domain.PancakeStub;
 import hu.oe.pancakestore.serviceclient.exception.FacadeException;
-import hu.oe.pancakestore.ejbservice.facade.CustomerFacade;
+import hu.oe.pancakestore.ejbservice.facade.PancakeFacade;
 
-@WebServlet("/CustomerList")
-public class CustomerListView extends HttpServlet {
+@WebServlet("/PancakeList")
+public class PancakeListView extends HttpServlet {
 
-	private static final long serialVersionUID = -1977646750178615187L;
 
-	private static final Logger LOGGER = Logger.getLogger(CustomerListView.class);
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5602073795060620225L;
 
-	private static final String ATTRIBUTE_CUSTOMERS = "customers";
-	private static final String PAGE = "customer-list.jsp";
+	private static final Logger LOGGER = Logger.getLogger(PancakeListView.class);
+
+	private static final String ATTRIBUTE_PANCAKES = "pancakes";
+	private static final String PAGE = "pancake-list.jsp";
 
 	@EJB
-	private CustomerFacade facade;
+	private PancakeFacade facade;
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		LOGGER.info("Get all customers");
+		LOGGER.info("Get all pancakes");
 		try {
-			final List<CustomerStub> customers = this.facade.getCustomers();
-			request.setAttribute(ATTRIBUTE_CUSTOMERS, customers);
+			final List<PancakeStub> pancakes = this.facade.getPancakes();
+			request.setAttribute(ATTRIBUTE_PANCAKES, pancakes);
 		} catch (final FacadeException e) {
 			LOGGER.error(e, e);
 		}
---
+++ projects/Javacsinta/jcs-weblayer/src/main/webapp/pancake-list.jsp
@@ -0,0 +1,59 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<%@ page import="java.util.Set"%>
+<%@ page import="java.util.List"%>
+<%@ page import="hu.oe.pancakestore.serviceclient.domain.PancakeStub"%>
+<%
+	List<PancakeStub> pancakes = (List<PancakeStub>) request.getAttribute("pancakes");
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<link rel="stylesheet" type="text/css" href="style/page.css" />
+<title>List of pancakes</title>
+</head>
+<body>
+	<div class="container">
+		<div class="header">
+			<h1>List of pancakes</h1>
+		</div>
+		<div class="nav">
+			<a href="http://localhost:8080/jcs-weblayer/">Home</a> <a
+				href="http://localhost:8080/jcs-weblayer/UILinks.jsp">Javacsinta
+				- user links</a> <a
+				href="http://localhost:8080/jcs-weblayer/MILinks.jsp">Javacsinta
+				- management links</a>
+		</div>
+		<div class="article">
+			<table class="pancakesstable">
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Price</th>
+					</tr>
+				</thead>
+				<tbody>
+					<%
+						for (PancakeStub pancake : pancakes) {
+					%>
+					<tr>
+						<td><a href="Pancake?name=<%out.print(pancake.getName());%>">
+								<%
+									out.print(pancake.getName());
+								%>
+						</a></td>
+						<td>"> <%
+							out.print(pancake.getPrice());
+						%>Ft
+						</td>
+					</tr>
+					<%
+						}
+					%>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</body>
+</html>
